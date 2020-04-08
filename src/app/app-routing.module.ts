03190423276eb5d78c714a823cfdef90a14454e7
IhorMaryash <ihormar@ukr.net>
2020-04-08 22:37:53 +0300

    project-netshop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './pages/home/home.component';
+import { MenComponent } from './pages/men/men.component';
+import { WomenComponent } from './pages/women/women.component';
+import { OutletComponent } from './pages/outlet/outlet.component';
+import { CollectionComponent } from './pages/collection/collection.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: 'home', component: HomeComponent},
+  // {path: '', pathMatch: 'full', redirectTo: ''},
+  // {path: '', component: HomeComponent},
+  {path: 'men', component: MenComponent},
+  {path: 'women', component: WomenComponent},
+  {path: 'outlet', component: OutletComponent},
+  {path: 'collection', component: CollectionComponent},
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenComponent } from './pages/men/men.component';
+import { WomenComponent } from './pages/women/women.component';
+import { OutletComponent } from './pages/outlet/outlet.component';
+import { CollectionComponent } from './pages/collection/collection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenComponent,
+    WomenComponent,
+    OutletComponent,
+    CollectionComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
